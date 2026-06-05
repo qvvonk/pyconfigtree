f2752c8ec632a35529e8449fb548cf94771e2e61
qvvonk <qvvonk@gmail.com>
2026-06-05 10:32:11 +0300

    default value

--- pyconfigtree/parameter/base.py
+++ pyconfigtree/parameter/base.py
@@ -4,7 +4,7 @@ from collections.abc import Callable
 
 
 T = TypeVar('T')
-
+UNSET = object()
 
 class Parameter(Node, Generic[T]):
     def __init__(
@@ -26,15 +26,27 @@ class MutableParameter(Parameter[T], Generic[T]):
     def __init__(
         self,
         node_id: str,
-        value: T,
-        default_value: T,
-        default_factory: Callable[[], T],
+        value: T = UNSET,
+        default_value: T = UNSET,
+        default_factory: Callable[[], T] = UNSET,
         name: str = '',
         description: str = '',
     ) -> None:
+        if default_value is UNSET and default_factory is UNSET:
+            raise ValueError('Default value or default factory must be set.')
+
+        self._default_factory = default_factory
+        self._default_value = default_value
+
         super().__init__(
             node_id=node_id,
-            value=value,
+            value=value if value is not UNSET else self.default_value,
             name=name,
             description=description
         )
+
+    @property
+    def default_value(self) -> T:
+        if self._default_factory is not UNSET:
+            return self._default_factory()
+        return self._default_value
