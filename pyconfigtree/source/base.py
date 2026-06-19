93bd945d4f49fd5d1de15995149d34d1b6c487e2
qvvonk <qvvonk@gmail.com>
2026-06-19 12:21:31 +0300

    node info for savers

--- pyconfigtree/source/base.py
+++ pyconfigtree/source/base.py
@@ -1,5 +1,15 @@
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, Union, TypeAlias
+from dataclasses import dataclass
+from enum import Enum, auto
+
+
+SIMPLE_TYPES: TypeAlias = Union[int, float, bool, str]
+CONTAINER_TYPES: TypeAlias = Union[
+    dict[str, Union[SIMPLE_TYPES, 'CONTAINER_TYPES']],
+    list[Union[SIMPLE_TYPES, 'CONTAINER_TYPES']],
+]
+ALLOWED_TYPES: TypeAlias = Union[SIMPLE_TYPES, CONTAINER_TYPES]
 
 
 class ConfigSource(ABC):
@@ -11,3 +21,21 @@ class ConfigSource(ABC):
 
     @abstractmethod
     def __eq__(self, other: Any) -> bool: ...
+
+
+class NodeType(Enum):
+    CONTAINER = auto()
+    LEAF = auto()
+
+
+@dataclass
+class NodeInfo:
+    name: str
+    description: str
+    type: NodeType
+    subnodes: dict[str, 'NodeInfo']
+    value: ALLOWED_TYPES | None = None
+
+    def __post_init__(self):
+        if self.type is NodeType.CONTAINER and self.value is not None:
+            raise ValueError(f'Node of type {self.type} cannot contain value.')
