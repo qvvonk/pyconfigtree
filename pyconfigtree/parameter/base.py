88f4592a1d5880a6a1ba9e711558956851d5498e
qvvonk <qvvonk@gmail.com>
2026-06-19 12:42:26 +0300

    set value

--- pyconfigtree/parameter/base.py
+++ pyconfigtree/parameter/base.py
@@ -1,8 +1,10 @@
 from ..base import Node
 from typing import Any, Generic, TypeVar
 from collections.abc import Callable
+from abc import ABC, abstractmethod
+from asyncio import Lock
 
-from ..source.base import NodeInfo, NodeType
+from ..source.base import NodeInfo, NodeType, ALLOWED_TYPES
 
 T = TypeVar('T')
 UNSET = object()
@@ -24,21 +26,22 @@ class Parameter(Node, Generic[T]):
         return self._value
 
 
-class MutableParameter(Parameter[T], Generic[T]):
+class MutableParameter(Parameter[T], ABC, Generic[T]):
     def __init__(
         self,
         node_id: str,
+        name: str = '',
+        description: str = '',
         value: T = UNSET,
         default_value: T = UNSET,
         default_factory: Callable[[], T] = UNSET,
-        name: str = '',
-        description: str = '',
     ) -> None:
         if default_value is UNSET and default_factory is UNSET:
             raise ValueError('Default value or default factory must be set.')
 
         self._default_factory = default_factory
         self._default_value = default_value
+        self._changing_lock = Lock()
 
         super().__init__(
             node_id=node_id,
@@ -59,5 +62,33 @@ class MutableParameter(Parameter[T], Generic[T]):
             name=self.name,
             description=self.description,
             type=NodeType.LEAF,
-            value=self.value
+            value=self.serialize()
         )
+
+    async def set_value(
+        self,
+        value: Any,
+        *,
+        skip_converter: bool = False,
+        skip_validator: bool = False,
+        skip_hook: bool = False,
+        save: bool = True,
+    ) -> None:
+        async with self._changing_lock:
+            if not skip_converter:
+                ...
+            if not skip_validator:
+                ...
+
+            self._value = value
+            if save:
+                ...
+
+        if not skip_hook:
+            ...
+
+    @abstractmethod
+    def serialize(self) -> ALLOWED_TYPES: ...
+
+    @abstractmethod
+    def deserialize(self, value: ALLOWED_TYPES) -> T: ...
