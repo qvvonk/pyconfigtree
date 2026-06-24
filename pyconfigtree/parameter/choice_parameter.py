bf110938323e13cdb6e4520e7e407febb7f4abb9
qvvonk <qvvonk@gmail.com>
2026-06-24 14:13:37 +0300

    choice

---
+++ pyconfigtree/parameter/choice_parameter.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from typing import Generic, TypeVar
+
+
+T = TypeVar('T')
+
+
+@dataclass(kw_only=True)
+class Choice(Generic[T]):
+    id: str
+    name: str = ''
+    description: str = ''
+    value: T
+
+    def __post_init__(self):
+        if not self.id:
+            raise ValueError('Choice ID cannot be empty.')
+
+        if not isinstance(id, str):
+            raise TypeError('Choice ID must be a string.')
+
+
