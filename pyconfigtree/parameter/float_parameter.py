b639ca83f76409c0ce6287dea17d64e20ad7f135
qvvonk <qvvonk@gmail.com>
2026-06-24 12:54:36 +0300

    int, float, string parameter

--- pyconfigtree/parameter/float_parameter.py
+++ pyconfigtree/parameter/float_parameter.py
@@ -2,9 +2,9 @@ from __future__ import annotations
 
 
 __all__ = [
-    'str_serializer',
-    'str_deserializer',
-    'StringParameter',
+    'float_serializer',
+    'float_deserializer',
+    'FloatParameter',
 ]
 
 
@@ -15,26 +15,26 @@ from .base import MutableParameter, UNSET, Validator, Serializer, Deserializer, 
 from pyconfigtree.exceptions import DeserializationError, ValidationError
 
 
-def str_serializer(value: str) -> str:
+def float_serializer(value: float) -> float:
     return value
 
 
-def str_deserializer(value: Any) -> str:
-    return str(value)
+def float_deserializer(value: Any) -> float:
+    return float(value)
 
 
-class StringParameter(MutableParameter[str]):
+class FloatParameter(MutableParameter[float]):
     def __init__(
         self,
         *,
         node_id: str,
         name: str = '',
         description: str = '',
-        default_value: str = UNSET,
-        default_factory: Callable[[], str] = UNSET,
-        validator: Optional[Validator['StringParameter', str]] = None,
-        serializer: Serializer[str] | None = None,
-        deserializer: Deserializer[str] | None = None,
+        default_value: float = UNSET,
+        default_factory: Callable[[], float] = UNSET,
+        validator: Optional[Validator['FloatParameter', float]] = None,
+        serializer: Serializer[float] | None = None,
+        deserializer: Deserializer[float] | None = None,
         on_value_changed_hook: Optional[ON_PARAMETER_VALUE_CHANGED_HOOK] = None,
     ) -> None:
         super().__init__(
@@ -43,24 +43,24 @@ class StringParameter(MutableParameter[str]):
             description=description,
             default_value=default_value,
             default_factory=default_factory,
-            serializer=serializer if serializer is not None else str_serializer,
-            deserializer=deserializer if deserializer is not None else str_deserializer,
+            serializer=serializer if serializer is not None else float_serializer,
+            deserializer=deserializer if deserializer is not None else float_deserializer,
             validator=validator,
             on_value_changed_hook=on_value_changed_hook
         )
 
-    def deserialize(self, value: Any) -> str:
+    def deserialize(self, value: Any) -> float:
         res = super().deserialize(value)
-        if not isinstance(res, str):
+        if not isinstance(res, float):
             raise DeserializationError(
-                f'Deserialized value of `StringParameter` '
-                f'must be an instance of `str`, not {type(res)}.'
+                f'Deserialized value of `FloatParameter` '
+                f'must be an instance of `float`, not {type(res)}.'
             )
         return res
 
     async def validate(self, value: Any) -> None:
-        if not isinstance(value, str):
+        if not isinstance(value, float):
             raise ValidationError(
-                f'Value of `StringParameter` must be an instance of `str`, not {type(value)}.'
+                f'Value of `FloatParameter` must be an instance of `float`, not {type(value)}.'
             )
         return await (super().validate(value))
--- pyconfigtree/parameter/int_parameter.py
+++ pyconfigtree/parameter/int_parameter.py
@@ -2,9 +2,9 @@ from __future__ import annotations
 
 
 __all__ = [
-    'str_serializer',
-    'str_deserializer',
-    'StringParameter',
+    'int_serializer',
+    'int_deserializer',
+    'IntParameter',
 ]
 
 
@@ -15,26 +15,26 @@ from .base import MutableParameter, UNSET, Validator, Serializer, Deserializer, 
 from pyconfigtree.exceptions import DeserializationError, ValidationError
 
 
-def str_serializer(value: str) -> str:
+def int_serializer(value: int) -> int:
     return value
 
 
-def str_deserializer(value: Any) -> str:
-    return str(value)
+def int_deserializer(value: Any) -> int:
+    return int(value)
 
 
-class StringParameter(MutableParameter[str]):
+class IntParameter(MutableParameter[int]):
     def __init__(
         self,
         *,
         node_id: str,
         name: str = '',
         description: str = '',
-        default_value: str = UNSET,
-        default_factory: Callable[[], str] = UNSET,
-        validator: Optional[Validator['StringParameter', str]] = None,
-        serializer: Serializer[str] | None = None,
-        deserializer: Deserializer[str] | None = None,
+        default_value: int = UNSET,
+        default_factory: Callable[[], int] = UNSET,
+        validator: Optional[Validator['IntParameter', int]] = None,
+        serializer: Serializer[int] | None = None,
+        deserializer: Deserializer[int] | None = None,
         on_value_changed_hook: Optional[ON_PARAMETER_VALUE_CHANGED_HOOK] = None,
     ) -> None:
         super().__init__(
@@ -43,24 +43,24 @@ class StringParameter(MutableParameter[str]):
             description=description,
             default_value=default_value,
             default_factory=default_factory,
-            serializer=serializer if serializer is not None else str_serializer,
-            deserializer=deserializer if deserializer is not None else str_deserializer,
+            serializer=serializer if serializer is not None else int_serializer,
+            deserializer=deserializer if deserializer is not None else int_deserializer,
             validator=validator,
             on_value_changed_hook=on_value_changed_hook
         )
 
-    def deserialize(self, value: Any) -> str:
+    def deserialize(self, value: Any) -> int:
         res = super().deserialize(value)
-        if not isinstance(res, str):
+        if not isinstance(res, int):
             raise DeserializationError(
-                f'Deserialized value of `StringParameter` '
-                f'must be an instance of `str`, not {type(res)}.'
+                f'Deserialized value of `IntParameter` '
+                f'must be an instance of `int`, not {type(res)}.'
             )
         return res
 
     async def validate(self, value: Any) -> None:
-        if not isinstance(value, str):
+        if not isinstance(value, int):
             raise ValidationError(
-                f'Value of `StringParameter` must be an instance of `str`, not {type(value)}.'
+                f'Value of `IntParameter` must be an instance of `int`, not {type(value)}.'
             )
         return await (super().validate(value))
