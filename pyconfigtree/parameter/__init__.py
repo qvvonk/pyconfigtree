60af5ceab1622875ab7e3240e3b2a3c20f5e404b
qvvonk <qvvonk@gmail.com>
2026-06-29 12:30:23 +0300

    parameter api

--- pyconfigtree/parameter/__init__.py
+++ pyconfigtree/parameter/__init__.py
@@ -1,2 +1,6 @@
 from .base import Parameter, MutableParameter
 from .bool_parameter import BoolParameter
+from .int_parameter import IntParameter
+from .float_parameter import FloatParameter
+from .string_parameter import StringParameter
+from .choice_parameter import Choice
