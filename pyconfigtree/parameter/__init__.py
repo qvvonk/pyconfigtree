04c0fedf13eea68aed3454e8ea9e285ac02d1c5c
qvvonk <qvvonk@gmail.com>
2026-07-01 12:59:37 +0300

    added choice param and list param to __init__

--- pyconfigtree/parameter/__init__.py
+++ pyconfigtree/parameter/__init__.py
@@ -5,5 +5,6 @@ from .base import (
 from .int_parameter import IntParameter as IntParameter
 from .bool_parameter import BoolParameter as BoolParameter
 from .float_parameter import FloatParameter as FloatParameter
-from .choice_parameter import Choice as Choice
+from .choice_parameter import Choice as Choice, ChoiceParameter as ChoiceParameter
+from .list_parameter import ListParameter as ListParameter
 from .string_parameter import StringParameter as StringParameter
