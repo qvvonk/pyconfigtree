f7572e329f4cfb4bdd7c39f9a799281de040c49d
qvvonk <qvvonk@gmail.com>
2026-07-03 13:40:33 +0300

    fixed default_value / default_factory logic

--- pyconfigtree/parameter/base.py
+++ pyconfigtree/parameter/base.py
@@ -124,9 +124,9 @@ class MutableParameter(Parameter[T], Generic[T]):
         on_value_changed_hook: ON_PARAMETER_VALUE_CHANGED_HOOK | None = None,
         flags: set[Any] | None = None,
     ) -> None:
-        if value is None and default_value is None:
+        if default_value is None and default_value is None:
             raise ValueError('Either `default_value` or `default_factory` must be specified.')
-        if value is not None and default_factory is not None:
+        if default_value is not None and default_factory is not None:
             raise ValueError(
                 'Either `default_value` or `default_factory` must be specified, '
                 'but not both of them.',
